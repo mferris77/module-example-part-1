a1df51667b1d2047a7390148f42c6f5ed921d7f6
sb <mferris77@gmail.com>
2020-07-13 06:09:07 -0400

    dunno

--- src/main.js
+++ src/main.js
@@ -3,16 +3,20 @@ import Router from './services/router'
 import Store from './services/store'
 import App from './App.vue'
 import system from './modules/system/module'
+import vuetify from './plugins/vuetify';
 
 /* Initialize System Module */
 Store.registerModule('system', system.store)
 Router.addRoutes(system.routes)
 Store.dispatch('system/initialize', null, { root: true })
 
-new Vue({ // eslint-disable-line no-new
-	el: '#app',
-	template: '<App/>',
-	router: Router,
-	store: Store,
-	render: h => h(App)
+new Vue({
+    // eslint-disable-line no-new
+    el: '#app',
+
+    template: '<App/>',
+    router: Router,
+    store: Store,
+    vuetify,
+    render: h => h(App)
 })
